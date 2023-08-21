0d0b06c38610498c75d6ba40e374fac44afb8b28
Nick Fagan <nick@fagan.org>
2023-08-21 14:32:16 -0400

    add pupil size + trail of past looking

--- example_overlay_gaze_on_videos.m
+++ example_overlay_gaze_on_videos.m
@@ -7,8 +7,12 @@ f = f.(char(fieldnames(f)));
 
 clip_table = f.clip_table;
 sync_info = extract_edf_sync_info( e.Events.Messages, f.edf_sync_times );
+pupil_threshs = pupil_limits( e.Samples.pupilSize );
 
 %%
+
+position_trail = ptb.Reference( struct('history', []) );
+context = ptb.Reference( struct('position_trail', position_trail) );
 
 begin = 1;
 max_num_clips = 20;
@@ -22,7 +26,7 @@ for i = begin:begin+min(max_num_clips, size(clip_table, 1))-1
   curr_clip = clip_table(i, :);
   vid_file_p = char( fullfile(vid_p, curr_clip.video_filename) );
   
-  draw_cb = @(t) overlay_gaze( win, e, sync_info, i, t );
+  draw_cb = @(t) overlay_gaze( win, context, e, sync_info, i, t, pupil_threshs );
   play_movie( win, vid_file_p, curr_clip.start, curr_clip.stop, [], draw_cb );
 end
 
@@ -30,11 +34,48 @@ close( win );
 
 %%
 
-function overlay_gaze(win, edf, sync_info, clip_index, vid_t)
+function threshs = pupil_limits(samples, props, num_bins)
+
+if ( nargin < 3 )
+  num_bins = 100;
+end
+
+if ( nargin < 2 )
+  props = [ 0.125, 0.85 ];
+end
+
+[h, edges] = histcounts( samples, num_bins );
+y = cumsum( h );
+y = y ./ y(end);
+
+ind0 = find( y > props(1), 1 );
+ind1 = find( y > props(2), 1 );
+threshs = [ edges(ind0), edges(ind1) ];
+
+end
+
+function update_trail(trail, curr_p, trail_len)
+
+tv = trail.Value;
+
+if ( size(tv.history, 1) < trail_len )
+  tv.history(end+1, :) = curr_p;
+else
+  tv.history(1:end-1) = tv.history(2:end);
+  tv.history(end, :) = curr_p;
+end
+
+trail.Value = tv;
+
+end
+
+function overlay_gaze(win, context, edf, sync_info, clip_index, vid_t, pupil_threshs)
 
 match_clip = sync_info(sync_info.clip_index == clip_index, :);
 
-if ( isempty(match_clip) || vid_t < min(match_clip.video_time) || vid_t > max(match_clip.video_time) )
+if ( isempty(match_clip) || ...
+     vid_t < min(match_clip.video_time) || ...
+     vid_t > max(match_clip.video_time) )
   return
 end
 
@@ -58,11 +99,35 @@ y0 = edf.Samples.posY(edf_t0_ind);
 x1 = edf.Samples.posX(edf_t1_ind);
 y1 = edf.Samples.posY(edf_t1_ind);
 
+ps0 = edf.Samples.pupilSize(edf_t0_ind);
+ps1 = edf.Samples.pupilSize(edf_t1_ind);
+
 x = (1 - f0) * x0 + f0 * x1;
 y = (1 - f0) * y0 + f0 * y1;
+ps = (1 - f0) * ps0 + ps1 * y1;
 
+ps = max( min(ps, pupil_threshs(2)), pupil_thresh(1) );
+psf = (ps - pupil_threshs(1)) ./ (pupil_threshs(2) - pupil_threshs(1));
 s = 25;
-r = [ x - s, y - s, x + s, y + s ];
-Screen( 'FillOval', win.WindowHandle, [255, 0, 0], r );
+
+Screen( 'FillOval', win.WindowHandle, [255, 0, 0], make_rect(x, y, s, psf) );
+
+% Draw history of gaze positions
+pt = context.Value.position_trail.Value.history;
+for i = 1:size(pt, 1)
+  prev_p = pt(i, :);
+  frac_p = (i - 1) / max( 1, size(pt, 1) - 1 );
+  Screen( 'FillOval', win.WindowHandle, [255 * frac_p, 0, 0] ...
+    , make_rect(prev_p(1), prev_p(2), s, prev_p(3)) );
+end
+
+trail_len = 10;
+update_trail( context.Value.position_trail, [x, y, psf], trail_len );
+
+function r = make_rect(x, y, s, psf)
+  adj_s = s * 0.5;
+  s = s + adj_s * (psf * 2 - 1);
+  r = [ x - s, y - s, x + s, y + s ];    
+end
 
 end
--- fv_data_directory.m
+++ fv_data_directory.m
@@ -1,5 +1,15 @@
-function p = fv_data_directory()
+function p = fv_data_directory(set_to)
 
-p = 'C:\source\data\free_viewing';
+persistent data_dir;
+
+if ( isempty(data_dir) )
+  data_dir = 'C:\source\data\free_viewing';
+end
+
+if ( nargin > 0 )
+  data_dir = set_to;
+end
+
+p = data_dir;
 
 end
